ddb253e3865bcaf5471a2cf3667aa89c48036aff
unknown <domgjonikrist@gmail.com>
2020-05-30 22:35:31 +0200

    adding comments

--- src/app/flash/flash.component.ts
+++ src/app/flash/flash.component.ts
@@ -10,11 +10,15 @@ import { IFlash } from "./../flash.model";
 
 export class FlashComponent implements OnInit {
   // tslint:disable-next-line: no-output-on-prefix
+
+  // We use @Output Decorator to emit events from child to parent
   @Output() onToggleCard = new EventEmitter();
   @Output() onDelete = new EventEmitter();
   @Output() onEdit = new EventEmitter();
   @Output() onRememberedChange = new EventEmitter();
 
+
+  // We use @Input Decorator to connect parent with child
   @Input() flash: IFlash = {
     id: 0,
     question: '',
